489f957359f161f487d449e64ad6b22766a841db
poing1020 <d.cassmagiacolor@hotmail.com>
2025-06-10 12:41:36 -0600

    searchBar funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { useState, useEffect } from 'react'
-import './App.css'
 import ImageCard from './components/ImageCard'
+import SearchBar from './components/SearchBar'
+import './App.css'
 
-function App() {
-  const[gifs, setGifs] = useState([])
+function App () {
+  const [gifs, setGifs] = useState([]) // Lista de Gifs
   const apiKey = import.meta.env.VITE_GIPHY_API_KEY
+  // Las llamadas a API normalmente se hacen en el useEffect
+  // Usaremos useEffect con un arreglo de dependencias vacío para que se ejecute solo una vez
 
-  useEffect(() => {
-    fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=30&offset=0&rating=g&bundle=messaging_non_clips`)
+  const sendSearch = (search) => {
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${search}&limit=25&offset=0&rating=g&lang=en`)
       .then(response => response.json())
       .then((results) => {
         console.log(results.data)
@@ -15,28 +18,36 @@ function App() {
       }).catch((error) => {
         console.error(error)
       })
+  }
 
-
-
+  useEffect(() => {
+    fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=25&rating=g`)
+      .then(response => response.json())
+      .then((results) => {
+        console.log(results.data)
+        setGifs(results.data)
+      }).catch((error) => {
+        console.error(error)
+      })
   }, [])
 
   return (
     <>
-    <div className='App'>
-      <div className='grid-cards'>
-        {
-          gifs.map((gif) => (
-            <ImageCard
-            key={gif.id}
-            url={gif.images.fixed_height.url}
-            title={gif.title}
-            />
-          ))
-        }
+      <div className='App'>
+        <SearchBar handleSearch={sendSearch} />
 
+        <div className='grid-cards'>
+          {
+            gifs.map(gif => (
+              <ImageCard
+                key={gif.id}
+                url={gif.images.fixed_height.url}
+                title={gif.title}
+              />
+            ))
+          }
+        </div>
       </div>
-
-    </div>
     </>
   )
 }
